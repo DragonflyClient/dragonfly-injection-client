e655d0aa72b409615a99e47eac77ca0480f5d84d
incxption <theincxption@gmail.com>
2020-12-10 22:10:18 +0100

    :wrench: Re-structure settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,3 @@
 rootProject.name = "dragonfly-injection-client"
-include("shared:agent", "shared:injection-hook", "1.8.8:minecraft", "1.8.8:injection-hook")
+include("shared:agent", "shared:injection-hook", "shared:obfuscator")
+include("1.8.8:minecraft", "1.8.8:injection-hook")

4e5dae5b24bb015f3eaf855aa284630b5ba30b93
incxption <theincxption@gmail.com>
2020-12-11 19:36:45 +0100

    :sparkles: Create dummy 1.8.8 injection hook

---
+++ 1.8.8/injection-hook/src/main/kotlin/net/dragonfly/injection188/CoreInjectionHook.kt
@@ -0,0 +1,38 @@
+package net.dragonfly.injection188
+
+import net.dragonfly.agent.DragonflyAgent
+import net.dragonfly.agent.dsl.InstrumentationWrapper
+import net.dragonfly.agent.dsl.editMethod
+import net.dragonfly.agent.hook.*
+import org.apache.logging.log4j.LogManager
+
+object CoreInjectionHook : InjectionHook() {
+    override val name: String = "Core Injection Hook Minecraft 1.8.8"
+
+    override fun premain(agent: DragonflyAgent) {
+        agent.loggingProvider = object : LoggingProvider {
+            override fun sendLog(message: String, level: Level) {
+                LogManager.getLogger("dragonfly-injector").log(translateLevel(level), message)
+            }
+
+            private fun translateLevel(input: Level): org.apache.logging.log4j.Level = when(input) {
+                Level.FATAL -> org.apache.logging.log4j.Level.FATAL
+                Level.ERROR -> org.apache.logging.log4j.Level.ERROR
+                Level.WARN -> org.apache.logging.log4j.Level.WARN
+                Level.INFO -> org.apache.logging.log4j.Level.INFO
+                Level.DEBUG -> org.apache.logging.log4j.Level.DEBUG
+                Level.TRACE -> org.apache.logging.log4j.Level.TRACE
+            }
+        }
+    }
+
+    override fun InstrumentationWrapper.configure() {
+        editClass("net.minecraft.client.gui.GuiMainMenu") {
+            editMethod("switchToRealms") {
+                setBody("""
+                    this.r = "heyho minecraft freunde!";
+                """.trimIndent())
+            }
+        }
+    }
+}

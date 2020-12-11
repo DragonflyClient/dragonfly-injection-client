21171a386a74951b0f53ceccb1eac5c7684cd114
incxption <theincxption@gmail.com>
2020-12-11 19:36:33 +0100

    :sparkles: Create dummy shared injection hook

---
+++ shared/injection-hook/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    java
+    kotlin("jvm") version "1.4.20"
+}
+
+group = "net.dragonfly"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly(kotlin("stdlib-jdk8"))
+
+    compileOnly("org.javassist:javassist:3.21.0-GA")
+    compileOnly("org.ow2.asm:asm:9.0")
+    compileOnly("org.ow2.asm:asm-tree:9.0")
+    compileOnly("org.ow2.asm:asm-util:9.0")
+
+    compileOnly(project(":shared:agent"))
+}
+
+tasks {
+    jar {
+        archiveFileName.set("shared-injection-hook.jar")
+    }
+}

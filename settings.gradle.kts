ae5af83171acce8ff09e58618a71f60ebb72e637
incxption <theincxption@gmail.com>
2020-12-12 17:52:05 +0100

    :sparkles: Add support for Minecraft 1.16.4

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,4 @@ rootProject.name = "dragonfly-injection-client"
 include("shared:agent", "shared:injection-hook", "shared:obfuscator", "shared:mapping-index-compiler")
 include("1.8.8:minecraft", "1.8.8:injection-hook")
 include("1.12:minecraft", "1.12:injection-hook")
+include("1.16.4:minecraft", "1.16.4:injection-hook")

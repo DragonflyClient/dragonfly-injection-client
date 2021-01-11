b1b04f47bf57bb489783e86fcf7f00944e2a4c99
moritz <theincxption@gmail.com>
2021-01-11 23:21:00 +0100

    :wrench: Adjust .gitignore and settings.gradle.kts to match new editions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,4 @@
 rootProject.name = "dragonfly-injection-client"
 include("shared:agent", "shared:dragonfly-core", "shared:obfuscator", "shared:mapping-index-compiler")
-include("1.8.8-octane:minecraft", "1.8.8-octane:injection-hook")
-include("1.12-vision:minecraft", "1.12-vision:injection-hook")
+include("1.8.9-octane:minecraft", "1.8.9-octane:injection-hook")
 include("1.16.4-vortex:minecraft", "1.16.4-vortex:injection-hook")

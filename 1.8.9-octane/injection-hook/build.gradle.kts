c5765dab7cb56481d816cb680f51db6e5f0a12ec
moritz <theincxption@gmail.com>
2021-01-11 23:10:55 +0100

    :fire: Add Minecraft Version 1.8.9 with OptiFine

---
+++ 1.8.9-octane/injection-hook/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    java
+    kotlin("jvm") version "1.4.20"
+}
+
+group = "net.dragonfly"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    compileOnly(kotlin("stdlib-jdk8"))
+
+    compileOnly("org.javassist:javassist:3.21.0-GA")
+    compileOnly("org.ow2.asm:asm:9.0")
+    compileOnly("org.ow2.asm:asm-tree:9.0")
+    compileOnly("org.ow2.asm:asm-util:9.0")
+
+    compileOnly("org.koin:koin-core:2.2.0")
+
+    compileOnly(project(":shared:agent"))
+    compileOnly(project(":shared:obfuscator"))
+    compileOnly(project(":shared:dragonfly-core"))
+    compileOnly(project(":1.8.9-octane:minecraft"))
+    compileOnly(fileTree("../minecraft/libraries/"))
+}
+
+tasks {
+    jar {
+        archiveFileName.set("injection-hook-1.8.9.jar")
+    }
+
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
---
+++ 1.8.9-octane/injection-hook/src/main/kotlin/net/dragonfly/octane/DragonflyOctane.kt
@@ -0,0 +1,18 @@
+package net.dragonfly.octane
+
+import net.dragonfly.agent.DragonflyAgent
+import net.dragonfly.agent.dsl.InstrumentationWrapper
+import net.dragonfly.agent.hook.*
+import net.dragonfly.octane.logging.OctaneLoggingProvider
+
+object DragonflyOctane : InjectionHook() {
+    override val name: String = "Dragonfly Octane Edition for Minecraft 1.8.9"
+    override val simpleName: String = "dragonfly-octane"
+
+    override fun premain(agent: DragonflyAgent) {
+        OctaneLoggingProvider.inject(agent)
+    }
+
+    override fun InstrumentationWrapper.configure() {
+    }
+}
